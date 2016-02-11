20e19bd306f12e645319080c4e54ba7750975001
matthewbbrumley <matt@syntropy.io>
2016-02-11 16:07:00 -0500

    First Commit

---
+++ welp/Welp.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    
+    //Variable Declaration
+    
+    //Method Declaration
+    //main is the function that runs first and starts the lifecycle
+    public static void main(String[] args) {
+        
+        //Print out good old Hello World!
+        
+    }
+    
+}
